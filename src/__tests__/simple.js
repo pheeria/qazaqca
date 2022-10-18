1073eb0568a9ce953b5d912c683a5c9801676764
Olzhas Askar <olzhas.askar@deliveryhero.com>
2022-10-18 11:23:59 +0200

    Implemented official gov version (#1)

--- src/__tests__/simple.js
+++ src/__tests__/simple.js
@@ -4,3 +4,13 @@ import {transliterate} from "../transliterator.js"
 test("Test simple cases", t => {
     t.is(transliterate("сапар"), "sapar")
 })
+
+test("ROFLs from the previous versions", t => {
+    t.not(transliterate("сәбіз"), "saebiz")
+    t.not(transliterate("нәби"), "naebi")
+})
+
+test("Bleeding eyes", t => {
+    t.is(transliterate("ыңғайсыз"), "yƞğaisyz")
+    t.is(transliterate("уайымдама"), "uaiymdama")
+})
